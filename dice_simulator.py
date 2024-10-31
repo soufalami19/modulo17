3b1df6d6774542ba956ae963ebf9a890a28cea4a
Soufiane Alami <souf@Air-de-Soufiane.lan>
2024-10-31 19:29:23 +0100

    Agregar codigo para simular lanzamiento de dados

---
+++ dice_simulator.py
@@ -0,0 +1,12 @@
+import dice
+import time
+
+def lanzar_dados(amount, sides):
+    resultados = [dice.roll(f'1d{sides}') for _ in range(amount)]
+    for i, resultado in enumerate(resultados, start=1):
+        print(f"Lanzamiento {i} número obtenido {resultado}")
+        time.sleep(5)
+    return resultados
+
+lanzar_dados(amount=5, sides=6)
+
