c2cdcd388dcf14a2eea3be4ec4e10674689049d6
Soufiane Alami <souf@Air-de-Soufiane.lan>
2024-10-31 19:31:45 +0100

    Cambiar mensaje de salida en el lanzamiento de dados

--- dice_simulator.py
+++ dice_simulator.py
@@ -4,7 +4,7 @@ import time
 def lanzar_dados(amount, sides):
     resultados = [dice.roll(f'1d{sides}') for _ in range(amount)]
     for i, resultado in enumerate(resultados, start=1):
-        print(f"Lanzamiento {i} número obtenido {resultado}")
+        print(f"Resultado del lanzamiento numero {i}: ha dado el resultado :  {resultado}")
         time.sleep(5)
     return resultados
 
